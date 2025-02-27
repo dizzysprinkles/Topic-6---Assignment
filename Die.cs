4d110fd6e352f370a68acd410c0d1c1b573658c0
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-27 14:38:53 -0500

    Finished Assignment

---
+++ Die.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic_6___Assignment
+{
+    public class Die
+    {
+        private Random _generator;
+        private int _roll;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+
+        public Die(int roll)
+        {
+            _generator = new Random();
+
+            if (roll < 1)
+            {
+                _roll = 1;
+            }
+            else if (roll > 6)
+            {
+                _roll = 6;
+            }
+            else
+            {
+
+                _roll = roll;
+            }
+        }
+
+        public int Roll
+        {
+            get { return _roll; }
+            //set { _roll = value; }
+        }
+
+        public override string ToString() // When trying to print die2 w/out .Roll, it will print the roll # instead of the namespace
+        {
+            return _roll + "";
+        }
+
+        public void RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+        }
+
+        public void DrawDie()
+        {
+            if (_roll == 1)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 2)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 3)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 4)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 5)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+
+            }
+            else if (_roll == 6)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("|o o|");
+                Console.WriteLine("-----");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,30 +8,193 @@ namespace Topic_6___Assignment
     {
         public static void Prompter()
         {
-            int max, min;
-            Console.WriteLine("Give me a maxmimum value");
-            Int32.TryParse(Console.ReadLine(), out max);
-            Console.WriteLine("Max is " + max);
-            // Check for if min is biiger than max or if max is smaller than min (which ever is asked first)
-            // Take in min, max
-            // Ask for ## between values, inclusive, integers
-            // Keep going until user inputs a correct number
+            int max, min, guess;
+            Console.Clear();
+            Console.WriteLine("Hi there! I will ask you for a maximum value and a minimum value and then you'll need to pick a number between them!");
+            Console.WriteLine("Please give me the maxmimum value: ");
+            while (!Int32.TryParse(Console.ReadLine(), out max))
+            {
+                Console.WriteLine("Invalid maximum. Try again!");
+            }
+            Console.WriteLine();
 
-
+            Console.WriteLine("And now for the  minimum value: ");
+            while (!Int32.TryParse(Console.ReadLine(), out min) || min >= max)
+            {
+                Console.WriteLine("Invalid minimum. Try again!");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Now what number would you like to pick that is between them? ");
+            while (!Int32.TryParse(Console.ReadLine(), out guess) || guess > max || guess < min)
+            {
+                Console.WriteLine("Invalid guess. Try again!");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Awesome! {guess} is between {min} and {max}!");
         }
 
-        public static void Bank(string selection, double amount)
+        public static void Bank()
         {
-            //QUIT,  Transaction options - Deposit, withdrawal, bill payement, account balance update - in invalid, assume account balance update
-            // .75 per transaction
+            string selection = "";
+            double deposit, withdrawal, payement, charge, accountBalance, minimumAmount;
+            charge = 0.75;
+            accountBalance = 150.00;
+            
+
+
+            while (selection != "q" || selection != "quit")
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to BoB ATM. We charge $0.75 per transaction. \nPlease type the number OR name of the transaction you would like to perform:");
+                Console.WriteLine("(Invalid selections will result in balance update. Charges will still be applied)");
+                Console.WriteLine();
+                Console.WriteLine("1 - Deposit");
+                Console.WriteLine("2 - Withdrawal");
+                Console.WriteLine("3 - Bill payement");
+                Console.WriteLine("4 - Account balance update");
+                Console.WriteLine("Q - Quit");
+                Console.WriteLine();
+                selection = Console.ReadLine().ToLower().Trim();
+                Console.WriteLine();
+    
+                if (selection == "1" || selection == "deposit")
+                {
+                    Console.WriteLine("You have chosen 1 - Deposit.");
+                    Console.WriteLine();
+                    Console.WriteLine("How much money would you like to deposit? ");
+                    if (!Double.TryParse(Console.ReadLine(), out deposit) || deposit < 0)
+                    {
+                        accountBalance -= charge;
+                        Console.WriteLine();
+                        Console.WriteLine("Invalid amount. Transaction fee has been subtracted from your balance.");
+                        Console.WriteLine("Your new balance is " + accountBalance.ToString("C"));
+                        Console.WriteLine();
+                        Console.WriteLine("Press ENTER to return to transaction selection.");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        accountBalance = accountBalance - charge + deposit;
+                        Console.WriteLine($"Awesome! You deposited {deposit.ToString("C")}. \nWith the subtraction of the transaction fee, you have {accountBalance.ToString("C")} remaining.");
+                        Console.WriteLine();
+                        Console.WriteLine("Press ENTER to return to transaction selection.");
+                        Console.ReadLine();
+                    }
+
+                }
+                else if (selection == "2" || selection == "withdrawal")
+                {
+                    Console.WriteLine("You have chosen 2 - Withdrawal.");
+                    Console.WriteLine();
+                    Console.WriteLine("How much would you like to withdraw? ");
+                    if (!Double.TryParse(Console.ReadLine(), out withdrawal) || withdrawal < 0 || withdrawal > accountBalance-charge)
+                    {
+                        accountBalance -= charge;
+                        Console.WriteLine();
+                        Console.WriteLine("Invalid amount. Transaction fee has been subtracted from your balance.");
+                        Console.WriteLine("Your new balance is " + accountBalance.ToString("C"));
+                        Console.WriteLine();
+                        Console.WriteLine("Press ENTER to return to transaction selection.");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        accountBalance = accountBalance - charge - withdrawal;
+                        Console.WriteLine($"Alright! You withdrew {withdrawal.ToString("C")}. \nWith the subtraction of the transaction fee, you have {accountBalance.ToString("C")} remaining.");
+                        Console.WriteLine();
+                        Console.WriteLine("Press ENTER to return to transaction selection.");
+                        Console.ReadLine();
+                    }
+
+                }
+                else if (selection == "3" || selection == "bill payement")
+                {
+                    Console.WriteLine("You have chosen 3 - Bill payement.");
+                    Console.WriteLine();
+                    Console.WriteLine("How much is the bill you would like to pay off? ");
+
+                    if (!Double.TryParse(Console.ReadLine(), out payement) || payement < 0 || payement > accountBalance-charge)
+                    {
+                        accountBalance -= charge;
+                        Console.WriteLine();
+                        Console.WriteLine("Invalid amount. Transaction fee has been subtracted from your balance.");
+                        Console.WriteLine("Your new balance is " + accountBalance.ToString("C"));
+                        Console.WriteLine();
+                        Console.WriteLine("Press ENTER to return to transaction selection.");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        accountBalance = accountBalance - charge - payement;
+                        Console.WriteLine($"Congrats on paying your bills! You paid {payement.ToString("C")}. \nWith the subtraction of the transaction fee, you have {accountBalance.ToString("C")} remaining.");
+                        Console.WriteLine();
+                        Console.WriteLine("Press ENTER to return to transaction selection.");
+                        Console.ReadLine();
+                    }
+
+                }
+                else if (selection == "4" || selection == "account balance update")
+                {
+                    Console.WriteLine("You have chosen 4 - Account balance update.");
+                    Console.WriteLine();
+                    accountBalance -= charge;
+                    Console.WriteLine();
+                    Console.WriteLine("Transaction fee has been subtracted from your balance.");
+                    Console.WriteLine("Your new balance is " + accountBalance.ToString("C"));
+                    Console.WriteLine();
+                    Console.WriteLine("Press ENTER to return to transaction selection.");
+                    Console.ReadLine();
+
+                }
+                else if (selection == "q" || selection == "quit")
+                {
+                    Console.WriteLine("Thank you for your patronage at BoB ATM! ");
+                    return;
+                }
+                else
+                {
+                    accountBalance -= charge;
+                    Console.WriteLine("Invalid transaction selection. Transaction fee has been subtracted from your balance.");
+                    Console.WriteLine("Your new balance is " + accountBalance.ToString("C"));
+                    Console.WriteLine();
+                    Console.WriteLine("Press ENTER to return to transaction selection.");
+                    Console.ReadLine();
+                }
+
+            }
         }
 
         public static void Dice()
         {
-            // Dice class from before 
-            // Separate the displaying of each roll of the two dice using some blank space or 
-            //requiring an ENTER press. Make sure to draw each roll set. At the end, print the number of rolls that
-            //were required to get doubles.
+            Console.Clear();
+            int numberRolls;
+            Die die1, die2;
+            die1 = new Die();
+            die2 = new Die();
+            Console.WriteLine("Hello! In this section, you will roll some dice until you get doubles!");
+            Console.WriteLine("Press ENTER to roll your first pair!");
+            Console.ReadLine();
+
+            die1.DrawDie();
+            die2.DrawDie();
+            numberRolls = 1;
+
+            while (die1.Roll != die2.Roll)
+            {
+                Console.WriteLine("Oops! You didn't roll doubles! Press ENTER to reroll you dice.");
+                Console.ReadLine(); 
+                die1.RollDie();
+                die2.RollDie();
+                numberRolls++;
+                die1.DrawDie();
+                die2.DrawDie();
+                Console.WriteLine() ;
+            }
+            Console.WriteLine($"You got doubles! It took you {numberRolls} roll(s) to get doubles! Congrats!");
+            Console.WriteLine();
         }
 
         static void Main(string[] args)
@@ -41,7 +204,7 @@ namespace Topic_6___Assignment
             while (choice != "q")
             {
                 Console.Clear();
-                Console.WriteLine("Welcome to Topic 6's menu!  Please select an option to try out:");
+                Console.WriteLine("Welcome to Topic 6's Main Menu!  Please select an option to try out:");
                 Console.WriteLine();
                 Console.WriteLine("1 - Prompter");
                 Console.WriteLine("2 - Banking Machine");
@@ -57,11 +220,17 @@ namespace Topic_6___Assignment
                     Console.WriteLine("Hit ENTER to continue.");
                     Console.ReadLine();
                     Prompter();
+                    Console.WriteLine();
+                    Console.WriteLine("Hit ENTER to return to Main Menu!");
+                    Console.ReadLine();
                 }
                 else if (choice == "2")
                 {
                     Console.WriteLine("You chose option 2 - Banking Machine");
                     Console.WriteLine("Hit ENTER to continue.");
+                    Console.ReadLine();
+                    Bank();
+                    Console.WriteLine("Hit ENTER to return to Main Menu!");
                     Console.ReadLine();
                 }
                 else if (choice == "3")
@@ -69,6 +238,15 @@ namespace Topic_6___Assignment
                     Console.WriteLine("You chose option 3 - Doubles Roller");
                     Console.WriteLine("Hit ENTER to continue.");
                     Console.ReadLine();
+                    Dice();
+                    Console.WriteLine("Hit ENTER to return to Main Menu!");
+                    Console.ReadLine();
+
+                }
+                else if (choice == "q")
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    return;
 
                 }
 
